f4227c869f9222c92bd45a338bd4041bc5074247
Rionma <chauveaumarion4@gmail.com>
2019-01-19 19:02:44 +0100

    fsjkfgjs

---
+++ capteurs.py
@@ -0,0 +1,45 @@
+import json
+from time import sleep
+from mqttmpd import MQTTMPDController
+
+# instanciating a controller
+controller = MQTTMPDController(
+    mqtt_broker='mpd.lan',
+    mqtt_client_id='mpd_controller',
+    mqtt_topicbase='music',
+    mqtt_port=1883,
+    mpd_server='mpd.lan',
+    mpd_port=6600
+)
+
+# def on_mes(self, )
+
+mqttc = controller.mqtt_connect()
+mqttc.subscribe("laumio/status/advertise")
+mqttc.publish("laumio/all/discover")
+mqttc.subscribe("capteur_bp/status")
+mqttc.subscribe("capteur_bp/switch/1/state")
+mqttc.subscribe("capteur_bp/binary_sensor/1/state")
+
+cmd= {
+  'command': 'fill',
+  'rgb': [255, 0, 0]
+}
+cmd2 = {
+  'command': 'animate_rainbow'
+}
+cmd3 = {
+  'command': 'fill',
+  'rgb': [0, 0, 255]
+}
+
+#mqttc.publish("laumio/Laumio_10805F/json", json.dumps(cmd))
+#sleep(1)
+#mqttc.publish("laumio/Laumio_107DA8/json", json.dumps(cmd3))
+#sleep(1)
+#mqttc.publish("laumio/Laumio_104F03/json", json.dumps(cmd))
+
+mqttc.loop_forever()
+#mqttc.publish("laumio/all/json", json.dumps(cmd2))
+
+controller.loop_forever()
--- test.py
+++ test.py
@@ -4,31 +4,46 @@ from random import random, sample
 import json
 
 laumios = set()
-musicON = False
+addVol = 0
 
 def on_message(client, userdata, msg):
-    global musicON
+    global tmin, tmax
+    global musicVOL
     s = str(msg.payload)[2:-1]
     if msg.topic == "laumio/status/advertise":
         if s != "discover" and (s not in laumios):
             laumios.add(s)
     elif msg.topic == "remote/playp/state":
         if s == "ON":
-            client.publish("music/control/{}".format("pause" if musicON else "play"))
-            musicON = not musicON
+            client.publish("music/control/toggle")
+    elif msg.topic == "remote/minus/state":
+        if s == "ON":
+            client.publish("music/control/getvol")
+            tmin=time.time()
+        else:
+            dt= time.time()-tmin
     elif msg.topic == "music/status":
-        if s.startswith("state:"):
-            musicON = s == "state: play"
+        try:
+            musicVOL = int(s)
+        except:
+            pass
+    
+
+
+        
+
+
     else:
         print("Not traited:", msg.topic)
+    
+
 
 client = mqtt.Client()
 client.on_message = on_message
 client.connect("mpd.lan")
 client.subscribe("remote/playp/state")
+client.subscribe("remote/minus/state")
 client.subscribe("laumio/status/advertise")
-client.subscribe("music/status")
-client.publish("music/control/getstate")
 client.loop_start()
 client.publish("laumio/all/discover")
 # client.publish("music/control/pause")
